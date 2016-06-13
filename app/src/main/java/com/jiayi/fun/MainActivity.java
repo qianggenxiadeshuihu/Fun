0cca99891159f24570fc7ef27adcb8e4be948bb2
qianggenxiadeshuihu <hujiayi.he29@163.com>
2016-06-13 11:54:09 +0800

    add part of login

--- app/src/main/java/com/jiayi/fun/MainActivity.java
+++ app/src/main/java/com/jiayi/fun/MainActivity.java
@@ -2,12 +2,16 @@ package com.jiayi.fun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, R.string.main_page_toast, Toast.LENGTH_LONG).show();
     }
 }
